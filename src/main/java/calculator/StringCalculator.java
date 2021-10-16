2565544c087866c60f06d8b5d1da1d04558151d6
Ajinkya Jagadale <ajinkyajagadale125@gmail.com>
2021-10-16 15:17:47 +0530

    tested for "" and "1" and "1,2"

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,25 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 class StringCalculator {
 
     public int add(String input) {
+        if (input.equals("")){
+            return 0;
+        }
+        if (input.length()==1){
+            return Integer.parseInt(input);
+        }
+        if (input.length()>1){
+            Optional<Integer> z = Arrays.stream(input.split(",")).map(Integer::parseInt).reduce(Integer::sum);
+            return z.get();
+        }
+
+
         return 0;
     }
 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,9 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    @Test
+    public void sumOfTwoInteger(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
 }
