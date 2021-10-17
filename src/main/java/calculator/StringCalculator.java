a62a2178bdca1e0edd1966008334bc1a26bd0519
Ajinkya Jagadale <ajinkyajagadale125@gmail.com>
2021-10-17 11:32:34 +0530

    corrected indentations and used sonar suggestions

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,60 +6,63 @@ import java.util.stream.Stream;
 
 class StringCalculator {
 
-    int calledCount=0;
-    public int add(String input)  {
+    int calledCount = 0;
+
+    public int add(String input) {
         calledCount++;
-        if (input.equals("")){
+        if (input.equals("")) {
             return 0;
         }
-        String customDelimiter="";
-        if (input.startsWith("//")){
+        String customDelimiter = "";
+        if (input.startsWith("//")) {
             String delimiterString = input.substring(2, input.indexOf("\n"));
             List<String> delimiters = new ArrayList<>();
             if (delimiterString.contains("[")) {
-                Integer startIndex=null;
-                Integer endIndex;
-                boolean flag=false;
+                Integer startIndex = null;
+                int endIndex;
+                boolean flag = false;
                 for (int i = 0; i < delimiterString.length(); i++) {
-                    if(delimiterString.charAt(i)=='[' && !flag){
-                        startIndex=i;
-                        flag=true;
+                    if (delimiterString.charAt(i) == '[' && !flag) {
+                        startIndex = i;
+                        flag = true;
                     }
-                    if(delimiterString.charAt(i)==']' && flag){
-                        endIndex=i;
-                        flag=false;
+                    if (delimiterString.charAt(i) == ']' && flag) {
+                        endIndex = i;
+                        flag = false;
 
-                            delimiters.add(delimiterString.substring(startIndex + 1, endIndex));
+                        delimiters.add(delimiterString.substring(startIndex + 1, endIndex));
 
 
                     }
                 }
-                customDelimiter+=String.join("|" , delimiters);
-
+                customDelimiter += String.join("|", delimiters);
 
 
             }
             int newLineIndex = input.indexOf('\n');
             input = input.substring(newLineIndex + 1);
-            customDelimiter+= String.valueOf(input.charAt(newLineIndex-1));
+            customDelimiter += String.valueOf(input.charAt(newLineIndex - 1));
         }
 
-        if (input.length()==1){
+        if (input.length() == 1) {
             return Integer.parseInt(input);
         }
-        if (input.length()>1){
-            Stream<Integer> integerStream = Arrays.stream(input.split(",|\n" + customDelimiter)).map(Integer::parseInt);
+        if (input.length() > 1) {
+            Stream<Integer> integerStream = Arrays.stream(input.split(",|\n" + "\\|" + customDelimiter)).map(Integer::parseInt);
             List<Integer> neg = integerStream.filter(x -> x < 0).collect(Collectors.toList());
             if (!neg.isEmpty())
                 throw new RuntimeException("negatives not allowed - " + neg.stream().map(Objects::toString).collect(Collectors.joining(",")));
-            Optional<Integer> z = integerStream.filter(x->x<1001).reduce(Integer::sum);
-            return z.get();
+            Optional<Integer> z = integerStream.filter(x -> x < 1001).reduce(Integer::sum);
+            if (z.isPresent())
+                return z.get();
+
         }
 
 
         return 0;
     }
-    public int getCalledCount(){
+
+    public int getCalledCount() {
         return this.calledCount;
     }
 
